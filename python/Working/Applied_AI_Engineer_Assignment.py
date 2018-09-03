aee341ddcb77f3568618502c4846b9cce6cc2f36
adwsb <mysticknight.adwesh@gmail.com>
2018-09-03 17:04:34 +0530

    Added AI Assessment

--- python/Working/Applied_AI_Engineer_Assignment.py
+++ python/Working/Applied_AI_Engineer_Assignment.py
@@ -1,10 +1,34 @@
-import tensorflow as tf 
+import tensorflow as tf
+import numpy as np
 
-c = tf.constant([[1,2,3], [4,5,6]])
-d = tf.constant([[7,8], [9,10], [11,12]])
-e = tf.matmul(c,d)
+m = 5
+n = 2
+p = 6
 
-# sess = tf.Session()
-# result = sess.run(e)
+mat1 = np.random.randint(0,100,(m,n))
+mat2 = np.random.randint(0,100,(n,p))
 
-tf.Print(e,e)
+t1 = tf.convert_to_tensor(mat1,np.float32) 
+t2 = tf.convert_to_tensor(mat2,np.float32) 
+
+matrix_mul = tf.matmul(t1,t2)
+
+mat3 = np.random.randint(0,100,(m,n))
+mat4 = np.random.randint(0,100,(m,n))
+
+t3 = tf.convert_to_tensor(mat3,tf.float64) 
+t4 = tf.convert_to_tensor(mat4,tf.float64) 
+
+t3 = tf.reshape(t3, [-1])
+t4 = tf.reshape(t4,[-1])
+
+cov_xx = 1 / (tf.shape(t3)[0] - 1) * tf.reduce_sum((t3 - tf.reduce_mean(t3))**2)
+cov_yy = 1 / (tf.shape(t3)[0] - 1) * tf.reduce_sum((t4 - tf.reduce_mean(t4))**2)
+cov_xy = 1 / (tf.shape(t3)[0] - 1) * tf.reduce_sum((t3 - tf.reduce_mean(t4)) * (t4 - tf.reduce_mean(t4)))
+
+#create  tensorflow session
+with tf.Session() as sess:
+    print("Multiplication Matrix: \n",sess.run(matrix_mul))
+    sess.run([cov_xx, cov_yy, cov_xy])
+    cov = tf.constant([[cov_xx.eval(), cov_xy.eval()], [cov_xy.eval(),cov_yy.eval()]])
+    print("\nCovariance Matrix: \n",cov.eval())
